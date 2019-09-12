3490eccd16f1623024e2c0032dc0e8e20c266c4d
hocuri <hocceruser-github@gmx.de>
2019-09-12 10:27:25 +0200

    Fix crash (android.util.AndroidRuntimeException: Calling startActivity...)

--- src/superfreeze/tool/android/userInterface/FreezeShortcutActivity.kt
+++ src/superfreeze/tool/android/userInterface/FreezeShortcutActivity.kt
@@ -253,7 +253,7 @@ class FreezeShortcutActivity : Activity() {
 		private fun onAppCouldNotBeFrozen(context: Context) {
 			Log.w(TAG, "AppCouldNotBeFrozen, bringing FreezeShortcutActivity back to front")
 			val i = Intent(context.applicationContext, FreezeShortcutActivity::class.java)
-			i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+			i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
 			context.startActivity(i)
 		}
 	}
