4e5ace34a61aefd636a6af32596ce6d2dc5ef742
Hocuri <hocceruser-github@gmx.de>
2020-04-11 08:14:04 +0200

    Improve update script

--- publish-fd-update.sh
+++ publish-fd-update.sh
@@ -39,6 +39,7 @@ git add fastlane/metadata/android/en-US/changelogs/
 
 
 echo "Getting the list of apps on F-Droid"
+rm index-v1.jar index-v1.json
 wget https://f-droid.org/repo/index-v1.jar
 unzip index-v1.jar -x "M*"
 python3 <<EOF >src/superfreeze/tool/android/database/FDroidPackages.kt
